1412941406a6c17be22d68c7b8fa29c64b28bd3b
mohammed.m <heymega7@gmail.com>
2022-06-17 17:26:01 +0300

    From day 1 to 100

---
+++ lib/day 2.dart
@@ -0,0 +1,191 @@
+import 'package:flutter/material.dart';
+
+class Day2Screen extends StatefulWidget {
+  @override
+  State<Day2Screen> createState() => _Day2Screen();
+}
+
+class _Day2Screen extends State<Day2Screen> {
+  PageController? _pageController;
+  int totalPage = 4;
+  Color colorAmber = Colors.amber;
+
+  void _onScroll() {
+    print('sss');
+  }
+
+  @override
+  void initState() {
+    _pageController = PageController(
+      initialPage: 0,
+    )..addListener((_onScroll));
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        children: <Widget>[
+
+          makePage(image: 'assets/images/one.jpeg',
+              title: 'Yosemite National Park',
+              page: 1,
+              numberStar: 4,
+              colorStar: colorAmber,
+              description: 'Yosemite National Park is in California’s Sierra Nevada mountains. It’s famed for its giant, ancient sequoia trees, and for Tunnel View, the iconic vista of towering Bridalveil Fall and the granite cliffs of El Capitan and Half Dome.'),
+
+          makePage(image: 'assets/images/two.jpeg',
+              title: 'Golden Gate Bridge',
+              page: 2,
+              numberStar:   4,
+              colorStar: colorAmber,
+              description: 'The Golden Gate Bridge is a suspension bridge spanning the Golden Gate, the one-mile-wide strait connecting San Francisco Bay and the Pacific Ocean.'),
+
+          makePage(image: 'assets/images/three.jpeg',
+              title: 'Sedona',
+              page: 3,
+              colorStar: Colors.grey,
+              numberStar: 3,
+              description: 'Sedona is regularly described as one of America\'s most beautiful places. Nowhere else will you find a landscape as dramatically colorful.'),
+
+          makePage(image: 'assets/images/four.jpeg',
+              title: 'Savannah',
+              page: 4,
+              colorStar: Colors.grey,
+
+              numberStar: 3,
+              description:'Savannah, with its Spanish moss, Southern accents and creepy graveyards, is a lot like Charleston, South Carolina. But this city about 100 miles to the south has an eccentric streak.' ),
+        ],
+      ),
+    );
+  }
+
+  Widget makePage({image,title,description,page,numberStar,colorStar}) {
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage(image),
+          fit: BoxFit.fill,
+        ),
+      ),
+      child: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(begin: Alignment.bottomRight, stops: [
+            0.3,
+            0.9
+          ], colors: [
+            Colors.black.withOpacity(.9),
+            Colors.black.withOpacity(.2),
+          ]),
+        ),
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              SizedBox(
+                height: 40,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.baseline,
+                textBaseline: TextBaseline.alphabetic,
+                children: <Widget>[
+                  Text(
+                    page.toString(),
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    '/' + totalPage.toString(),
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 15,
+                    ),
+                  ),
+                ],
+              ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    Text(
+                      title,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 40,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Container(
+                          margin: EdgeInsets.only(right: 3),
+                          child: Icon(Icons.star,color: Colors.amber,size: 15,),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(right: 3),
+                          child: Icon(Icons.star,color: Colors.amber,size: 15,),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(right: 3),
+                          child: Icon(Icons.star,color: Colors.amber,size: 15,),
+                        ),
+                        Container(
+
+                          margin: EdgeInsets.only(right: 3),
+                          child: Icon(Icons.star,color: colorStar,size: 15,),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(right: 5),
+                          child: Icon(Icons.star,color: Colors.grey,size: 15,),
+                        ),
+                        Text(
+                          numberStar.toString(),
+                          style: TextStyle(
+                            color: Colors.white70,
+                            fontSize: 15,
+                          ),
+                        ),
+                        Text(
+                          '(2300)',
+                          style: TextStyle(
+                            color: Colors.white38,
+                            fontSize: 12,
+                          ),
+                        ),
+
+
+
+
+                      ],
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 50),
+                      child: Text(description,style: TextStyle(color: Colors.white.withOpacity(.7), height: 1.9, fontSize: 15),),
+                    ),
+                    SizedBox(height: 20,),
+                    Text('READ MORE',style: TextStyle(color: Colors.white),),
+                    SizedBox(height: 30,),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
