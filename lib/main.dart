3d41763964490f8b5e9f32ba068b98501edc0452
mohammed.m <heymega7@gmail.com>
2022-08-05 22:33:21 +0300

    From day 1 to 100

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
-import 'package:design_ui/day%202.dart';
-import 'package:design_ui/day%203.dart';
-import 'package:design_ui/day%204/Screens/day%204.dart';
-import 'package:design_ui/day%204/utils/constants.dart';
 import 'package:flutter/material.dart';
+
+import 'day 4 afg/day 4.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Screen4Day(),
+      home: Day4Screen(),
     );
   }
 }
